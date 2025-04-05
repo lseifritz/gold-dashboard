8b7bde979a80e27167dd65b9095a4697f85c9066
pierrengn <pierreng2003@gmail.com>
2025-04-05 15:33:55 +0000

    import data

--- report.py
+++ report.py
@@ -1,1 +1,11 @@
 
+import pandas as pd
+from datetime import datetime
+
+def load_data():
+    df = pd.read_csv("data.csv", names=["Date", "Price"])
+    df["Date"] = pd.to_datetime(df["Date"], format="%Y-%m-%d %H:%M:%S")
+    df["Price"] = df["Price"].astype(float)
+    return df
+
+df = load_data()
